596e5582a23f33d3265160f3f7200dcb3afa23cc
devSJR <stefan.roediger@hs-lausitz.de>
2014-09-09 00:12:49 +0200

    changed to match naming convention

--- R/summary.RDML.R
+++ R/summary.RDML.R
@@ -1,45 +1,45 @@
 summary.RDML_object <- function(object, print = TRUE, ...) {
-  dil_table <- do.call(rbind, object[["Dilutions"]])
-  rownames(dil_table) <- names(object[["Dilutions"]])
+  dilTable <- do.call(rbind, object[["Dilutions"]])
+  rownames(dilTable) <- names(object[["Dilutions"]])
   
-  exp_tab <- data.frame(do.call(rbind, lapply(object[["qPCR"]], function(i) {
+  expTab <- data.frame(do.call(rbind, lapply(object[["qPCR"]], function(i) {
     tab <- do.call(rbind, lapply(i, function(j)
       summary(MFIaggr(j), print = FALSE)))
     data.frame(type = rownames(tab), tab, row.names = NULL)
     })), row.names = NULL)
-  exp_tab <- cbind(experiment = as.vector(sapply(names(object[["qPCR"]]), function(i) 
-    rep(i, 2))), exp_tab) 
+  expTab <- cbind(experiment = as.vector(sapply(names(object[["qPCR"]]), function(i) 
+    rep(i, 2))), expTab) 
   cat("Dilutions:\n")
-  print(dil_table)
+  print(dilTable)
   
   #first three moments
-  first_three <- exp_tab[, c("experiment", "type", "mean", "sd", "skewness")]
-  colnames(first_three) <- c("Experiment", "Type", "Mean", "Standard deviation", "Skewness")
+  firstThree <- expTab[, c("experiment", "type", "mean", "sd", "skewness")]
+  colnames(firstThree) <- c("Experiment", "Type", "Mean", "Standard deviation", "Skewness")
   cat("\nSummarized experiments - first three moments:\n")
-  print(first_three)
+  print(firstThree)
   
   #resistant statistics
-  resist_stats <- exp_tab[, c("experiment", "type", "median", "mad", "IQR", "medcouple")]
-  colnames(resist_stats) <- c("Experiment", "Type", "Median", "Median Absolute Deviation", 
+  resistStats <- expTab[, c("experiment", "type", "median", "mad", "IQR", "medcouple")]
+  colnames(resistStats) <- c("Experiment", "Type", "Median", "Median Absolute Deviation", 
                              "Interquartile Range", "Medcouple")
   cat("\nSummarized experiments - resistant statistics:\n")
-  print(resist_stats)
+  print(resistStats)
   
   #linear
-  lin_stats <- exp_tab[, c("experiment", "type", "intercept", "slope", "r.squared", 
+  linStats <- expTab[, c("experiment", "type", "intercept", "slope", "r.squared", 
                            "heter.p")]
-  colnames(lin_stats) <- c("Experiment", "Type", "Intercept", "Slope", 
+  colnames(linStats) <- c("Experiment", "Type", "Intercept", "Slope", 
                            "R squared", "Breusch-Pagan Test p-value")
   cat("\nSummarized experiments - linear model statistics:\n")
-  print(lin_stats)
+  print(linStats)
   
   #rest
-  rest_stats <- exp_tab[, c("experiment", "type", "SNR", "VRM", "NAs")]
-  colnames(rest_stats) <- c("Experiment", "Type", "SNR", "VRM", "Number of NAs")
+  restStats <- expTab[, c("experiment", "type", "SNR", "VRM", "NAs")]
+  colnames(restStats) <- c("Experiment", "Type", "SNR", "VRM", "Number of NAs")
   cat("\nSummarized experiments - other statistics:\n")
-  print(rest_stats)
+  print(restStats)
   
   cat("\n")
   
-  invisible(list(dil_table = dil_table, exp_tab = exp_tab))
+  invisible(list(dilTable = dilTable, expTab = expTab))
 }
