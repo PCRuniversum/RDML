a51a4c37ab66196c5f1b5ee06bfe4248a47c00d7
kablag <k.blag@yandex.ru>
2014-10-10 16:14:39 +0400

    added names R3 to RDML

--- R/names.RDML.R
+++ R/names.RDML.R
@@ -1,6 +1,11 @@
-#'Names RDML R6 object
-#'
-#'
+#' The Names of the RDML object
+#' 
+#' Just wraps \code{ls()} function to show RDML public fields
+#' and methods. Useful for RStudio autocomplete.
+#' 
+#' @aliases names.RDML names,RDML-method
+#' @param object an object of class \code{RDML}.
+#' @export
 names.RDML <- function(object) {
   ls(object)
 }
