18e4405da439be6c68f4e305772bc4cc6af474b3
kablag <k.blag@yandex.ru>
2019-10-18 11:44:52 +0300

    test upd

--- tests/testthat/test_rdml_new.R
+++ tests/testthat/test_rdml_new.R
@@ -19,3 +19,21 @@ test_that("RDML can be created from LightCycler .rdml", {
   rdml <- RDML$new(filename)
   expect_class(rdml, "RDML")
 })
+
+test_that("RDML can be created from .csv", {
+  filename <- paste0(PATH, "/extdata/from_tables/", "fdata.csv")
+  rdml <- RDML$new(filename)
+  expect_class(rdml, "RDML")
+})
+
+test_that("RDML can be created from .xlsx", {
+  filename <- paste0(PATH, "/extdata/from_tables/", "table.xlsx")
+  rdml <- RDML$new(filename)
+  expect_class(rdml, "RDML")
+})
+
+test_that("RDML can be created from ABI7500 .eds", {
+  filename <- paste0(PATH, "/extdata/from_abi7500/", "sce.eds")
+  rdml <- RDML$new(filename)
+  expect_class(rdml, "RDML")
+})
